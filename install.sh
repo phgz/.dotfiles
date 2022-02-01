af2c0ac2efc42d594287a990e9449f58f6474498
Philip Gaudreau <this@philipgaudreau.email>
2022-02-01 17:39:02 -0500

    Change nvim installation from X.Y to nightly

--- install.sh
+++ install.sh
@@ -52,7 +52,7 @@ elif [ "$platform" == "Linux" ]; then
 
     #sudo apt install tmux
 
-    curl -LJO https://github.com/neovim/neovim/releases/download/v0.6.1/nvim.appimage
+    curl -LJO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
     chmod u+x nvim.appimage
     mv nvim.appimage $HOME/.local/bin/nvim
 fi
