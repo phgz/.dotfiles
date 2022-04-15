e686a8d2245f171d54a2b47174e2dd7fc2c661c5
Philip Gaudreau <pgaudreau@explorance.com>
2022-04-15 18:03:18 -0400

    WIP

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,7 +1,8 @@
 local utils = require('utils')
 utils.opt('o', 'termguicolors', true)
 
-vim.o.background = "dark"    -- Setting dark mode
+local time_of_day = tonumber(os.date("%H"))
+
 
 -- GRUVBOX_MATERIAL
 -- vim.g.gruvbox_material_background = 'medium'
@@ -15,8 +16,34 @@ vim.g.gruvbox_material_menu_selection_background = 'orange'
 vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
 vim.g.gruvbox_material_palette = 'original'
 
--- Change search hl to orange instead of red 
-vim.api.nvim_exec([[
-colorscheme gruvbox-material
-call gruvbox_material#highlight('IncSearch', ['#282828',   '235'], ['#fe8019',   '208'])
-]], false)
+if time_of_day >= 8 and time_of_day < 20 then
+  vim.o.background = light    -- Setting dark mode
+
+  require("github-theme").setup({
+    theme_style = "light",
+    function_style = "italic",
+    transparent = true,
+    hide_end_of_buffer = false,
+
+    -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+    colors = {hint = "orange", error = "#ff0000"},
+
+    -- Overwrite the highlight groups
+    -- overrides = function(c)
+    --   return {
+    --     htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
+    --     DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
+    --     -- this will remove the highlight groups
+    --     TSField = {},
+    --   }
+    -- end
+  })
+
+else
+  vim.o.background = dark    -- Setting dark mode
+  -- Change search hl to orange instead of red
+  vim.api.nvim_exec([[
+  colorscheme gruvbox-material
+  call gruvbox_material#highlight('IncSearch', ['#282828',   '235'], ['#fe8019',   '208'])
+  ]], false)
+end
--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,14 +1,15 @@
 local utils = require('utils')
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = 'maintained',
+  ensure_installed = 'all',
   highlight = {
     enable = true,
     disable = {"toml"},
     additional_vim_regex_highlighting = {'python'},
   },
   indent = {
-    enable = true
+    enable = false,
+    disable = { "python" }
   },
   rainbow = {
     enable = true,
@@ -148,22 +149,22 @@ require('nvim-treesitter.configs').setup {
   },
 }
 
-vim.api.nvim_exec([[
-highlight! TSDefinition gui=underline
-highlight! TSDefinitionUsage gui=bold
-
-highlight! link TreesitterContext CursorLine
-
-highlight! link TSKeywordFunction RedItalic
-highlight! link TSKeyword RedItalic
-highlight! link TSConditional RedItalic
-highlight! link TSRepeat RedItalic
-highlight! link TSString Green
-highlight! link TSInclude PreProc
-highlight! link TSField Blue
-highlight! link TSFuncBuiltin Yellow
-highlight! link TSFunction AquaBold
-highlight! link TSMethod Aqua
-]], false)
+-- vim.api.nvim_exec([[
+-- highlight! TSDefinition gui=underline
+-- highlight! TSDefinitionUsage gui=bold
+--
+-- highlight! link TreesitterContext CursorLine
+--
+-- highlight! link TSKeywordFunction RedItalic
+-- highlight! link TSKeyword RedItalic
+-- highlight! link TSConditional RedItalic
+-- highlight! link TSRepeat RedItalic
+-- highlight! link TSString Green
+-- highlight! link TSInclude PreProc
+-- highlight! link TSField Blue
+-- highlight! link TSFuncBuiltin Yellow
+-- highlight! link TSFunction AquaBold
+-- highlight! link TSMethod Aqua
+-- ]], false)
 
 utils.map('n', '<localleader>t', '<cmd>TSHighlightCapturesUnderCursor<cr>')
