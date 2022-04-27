85168616c33aa638da63cdef4b53fd12b91fc83e
Philip Gaudreau <pgaudreau@explorance.com>
2022-04-27 17:30:22 -0400

    Remove test for TMUX since /tmp/tmux-1007/default,3928574,0 is always empty in --login mode

--- fish/functions/theme.fish
+++ fish/functions/theme.fish
@@ -1,9 +1,8 @@
 function theme
-    if test -n "$TMUX"
-        cat $HOME/.dotfiles/tmux/$argv-theme.conf > $HOME/.dotfiles/tmux/current-theme.conf \
-        && tmux source-file $HOME/.dotfiles/tmux/current-theme.conf
-    else
-        kitty +kitten themes --reload-in=all Theme-$argv
-    end
+    cat $HOME/.dotfiles/tmux/$argv-theme.conf > $HOME/.dotfiles/tmux/current-theme.conf \
+    && tmux source-file $HOME/.dotfiles/tmux/current-theme.conf
+
+    kitty +kitten themes --reload-in=all Theme-$argv
+
     cat $HOME/.dotfiles/nvim/lua/config/$argv-theme.lua > $HOME/.dotfiles/nvim/lua/config/current-theme.lua
 end
