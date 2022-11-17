38b5bbdc6773f2d3686316a1a4b1adcf1da510e7
Philip Gaudreau <pgaudreau@explorance.com>
2022-11-17 02:55:24 -0500

    [nvim] Add uncomment textregion function

--- nvim/lua/config/comment.lua
+++ nvim/lua/config/comment.lua
@@ -1,21 +1,21 @@
 require("Comment").setup{
       toggler = {
-        ---Line-comment toggle keymap
-        line = 'KK',
+        --Line-comment toggle keymap
+        line = '<M-k>',
     },
 
-    ---LHS of operator-pending mappings in NORMAL + VISUAL mode
-    ---@type table
+    --LHS of operator-pending mappings in NORMAL + VISUAL mode
+    --@type table
     opleader = {
-        -- -Line-comment keymap
+        -- Line-comment keymap
         line = 'K',
     },
       extra = {
-        ---Add comment on the line above
+        --Add comment on the line above
         above = 'KO',
-        ---Add comment on the line below
+        --Add comment on the line below
         below = 'Ko',
-        ---Add comment at the end of line
+        --Add comment at the end of line
         eol = 'KA',
     },
 }
@@ -39,3 +39,42 @@ function()
 	vim.api.nvim_win_set_cursor(0, { srow+1, col-1 })
 end
 , { silent = true, noremap = true })
+
+ --Textobject for adjacent commented lines
+local function commented_lines_textobject()
+	local U = require("Comment.utils")
+	local current_line = vim.api.nvim_win_get_cursor(0)[1] -- current line
+	local range = { srow = current_line, scol = 0, erow = current_line, ecol = 0 }
+	local ctx = {
+		ctype = U.ctype.linewise,
+		range = range,
+	}
+	local cstr = require("Comment.ft").calculate(ctx) or vim.bo.commentstring
+	local ll, rr = U.unwrap_cstr(cstr)
+	local padding = true
+	local is_commented = U.is_commented(ll, rr, padding)
+
+	local line = vim.api.nvim_buf_get_lines(0, current_line - 1, current_line, false)
+	if next(line) == nil or not is_commented(line[1]) then
+		return
+	end
+
+	local rs, re = current_line, current_line -- range start and end
+	repeat
+		rs = rs - 1
+		line = vim.api.nvim_buf_get_lines(0, rs - 1, rs, false)
+	until next(line) == nil or not is_commented(line[1])
+	rs = rs + 1
+	repeat
+		re = re + 1
+		line = vim.api.nvim_buf_get_lines(0, re - 1, re, false)
+	until next(line) == nil or not is_commented(line[1])
+	re = re - 1
+
+  vim.api.nvim_buf_set_mark(0,'<', rs, 0, {})
+  vim.api.nvim_buf_set_mark(0,'>', re, 0, {})
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("gvV", true, false, true), 'x', true)
+end
+
+vim.keymap.set("o", "u", commented_lines_textobject,
+	{ silent = true, desc = "Textobject for adjacent commented lines" })
