59961ec941204b7c56f62712d8c99cf5e1d0f0eb
Philip Gaudreau <philip.gaudreau@icloud.com>
2023-12-22 13:00:25 -0500

    change indent char

--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -368,7 +368,7 @@ return {
 		-- main = "ibl",
 		config = function()
 			require("ibl").setup({
-				indent = { char = "▏" },
+				indent = { char = "│" },
 				scope = {
 					enabled = false,
 				},
