4a507f39327fa4fe8e78ae7ccd83f1cec73d9295
Philip Gaudreau <philip.gaudreau@icloud.com>
2024-01-05 18:42:38 -0500

    Open files in read-only mode on SwapExists

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -8,6 +8,23 @@ api.nvim_create_autocmd("FileType", {
 	command = [[nnoremap <buffer><silent> q :close<cr> | let &stc=""]],
 })
 
+api.nvim_del_augroup_by_name("nvim_swapfile")
+api.nvim_create_autocmd("SwapExists", {
+	callback = function()
+		local info = vim.fn.swapinfo(vim.v.swapname)
+		local user = vim.uv.os_get_passwd().username
+		local iswin = 1 == vim.fn.has("win32")
+		if info.error or info.pid <= 0 or (not iswin and info.user ~= user) then
+			vim.v.swapchoice = "" -- Show the prompt.
+			return
+		end
+
+		vim.v.swapchoice = "o" -- Choose "Read-Only".
+		vim.bo.modifiable = false
+		vim.notify("This file is already opened elsewhere. Opening in RO mode")
+	end,
+})
+
 api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
 		vim.highlight.on_yank()
