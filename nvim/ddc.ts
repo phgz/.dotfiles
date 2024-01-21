9517fa56867511a3bb2a0f4ff4fa1c3e1ab77015
Philip Gaudreau <philip.gaudreau@icloud.com>
2024-01-21 11:04:39 -0500

    remove `file` source, add `look` source for files without lsp

--- nvim/ddc.ts
+++ nvim/ddc.ts
@@ -5,7 +5,7 @@ export class Config extends BaseConfig {
   override config(args: ConfigArguments): Promise<void> {
     args.contextBuilder.patchGlobal({
       ui: "inline",
-      sources: ["lsp", "around", "file"],
+      sources: ["lsp", "around", "look"],
       autoCompleteEvents: [
         "InsertEnter",
         "TextChangedI",
@@ -14,39 +14,44 @@ export class Config extends BaseConfig {
       sourceOptions: {
         _: {
           matchers: ["matcher_fuzzy"],
-          sorters: ["sorter_fuzzy"],
+          // sorters: ["sorter_rank"],
           converters: ["converter_remove_overlap"],
           minAutoCompleteLength : 1,
         },
         around: {
-          mark: "A",
           enabledIf: "luaeval('vim.tbl_count(vim.lsp.get_clients()) == 0')",
         },
-        file: {
-          mark: "F",
-          forceCompletionPattern: "\\S/\\S*",
+        look: {
+          enabledIf: "luaeval('vim.tbl_count(vim.lsp.get_clients()) == 0')",
+          matchers: [
+              {
+                name: "matcher_fuzzy",
+                params: {splitMode: "char"}
+              }
+          ]
         },
         lsp: {
-          mark: "LSP",
           forceCompletionPattern: "\\.\\w*|::\\w*|->\\w*",
           sorters: ["sorter_lsp-kind"],
           dup: "force",
         },
       },
       sourceParams: {
-        "lsp": {
-          snippetEngine: "vim.snippet",
+        lsp: {
+          // snippetEngine: "vim.snippet", not implemented
           enableResolveItem: true,
           enableAdditionalTextEdit: true,
         },
             },
+      // postFilters:["sorter_rank"],
+      postFilters:["sorter_fuzzy"],
       filterParams: {
         matcher_fuzzy: {
           splitMode: "word",
         },
         "sorter_lsp-kind": {
           priority: [["Variable", "Text", "Method", "Function"]]
-        }
+        },
       },
       backspaceCompletion: true,
     });
