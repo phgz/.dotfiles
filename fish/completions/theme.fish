dfdc826346901d9d4bf7e30541e78b3c2619006c
Philip Gaudreau <philip.gaudreau@icloud.com>
2024-01-21 10:55:12 -0500

    refactor colorschemes events

--- fish/completions/theme.fish
+++ fish/completions/theme.fish
@@ -1,2 +1,2 @@
-complete -c theme -fa "day night"
+complete -c theme -fa "day twilight evening night"
 complete -c theme -l force -s f
--- fish/functions/theme.fish
+++ fish/functions/theme.fish
@@ -1,85 +1,48 @@
 function theme
     if test "$argv" = day
+        or test "$argv" = twilight
+        or test "$argv" = evening
         or test "$argv" = night
-        or test "$argv" = midnight
-        or test "$argv" = transition
         echo $argv >$HOME/.dotfiles/theme/current
         return 0
     end
 
     set -f current_theme_path $HOME/.dotfiles/theme/current
-    set -f system_mode (head -n 1 $current_theme_path)
+    set -f current_theme (head -n 1 $current_theme_path)
+    set -f current_time (date +%s)
+
+    if test (uname -s) = Linux
+        set -f night_start (date -d "$(date +%Y-%m-%d) 00:00:00" "+%s")
+        set -f day_start (date -d "$(date +%Y-%m-%d) 06:00:00" "+%s")
+    else
+        set -f night_start (date -j -f "%Y-%m-%d %H:%M:%S" "$(date +%Y-%m-%d) 00:00:00" "+%s")
+        set -f day_start (date -j -f "%Y-%m-%d %H:%M:%S" "$(date +%Y-%m-%d) 06:00:00" "+%s")
+    end
+
     set -f coordinates "@45.5031824 -73.5698065"
     set -f info (sunshine -f %s "$coordinates")
-    set -f sunrise (string split ' ' $info[1])[2]
-    set -f sunset (string split ' ' $info[2])[2]
-    if test (uname -s) = Linux
-        set -f midnight (date -d "$(date +%Y-%m-%d) 00:00:00" "+%s")
-    else
-        set -f midnight (date -j -f "%Y-%m-%d %H:%M:%S" "$(date +%Y-%m-%d) 00:00:00" "+%s")
-    end
-    set -f current (date +%s)
-
-    set -f choices day night midnight
-    set -f diffs
-
-    for event in $sunrise $sunset $midnight
-        set -a diffs (math $current - $event)
-    end
-    set -f dists
-
-    for diff in $diffs
-        set -a dists (math abs $diff)
-    end
-    set -f seeking $choices[1]
-    set -f dist $dists[1]
-    set -f diff $diffs[1]
-
-    for i in 2 3
-        if test $dists[$i] -lt $dist
-            set dist $dists[$i]
-            set seeking $choices[$i]
-            set diff $diffs[$i]
-        end
-    end
+    set -f evening_start (string split ' ' $info[2])[2]
+    set -f twilight_start (math $evening_start - 3600)
 
     set -f cycle
 
-    if test $current -ge $midnight
-        and test $current -lt $sunrise
-        set cycle midnight
-    else if test $current -ge $sunrise
-        and test $current -lt $sunset
-        set cycle day
-    else
+    if test $current_time -ge $night_start
+        and test $current_time -lt $day_start
         set cycle night
+    else if test $current_time -ge $day_start
+        and test $current_time -lt $twilight_start
+        set cycle day
+    else if test $current_time -ge $twilight_start
+        and test $current_time -lt $evening_start
+        set cycle twilight
+    else
+        set cycle evening
     end
-
-    # echo current $current >>/Users/philip/theme-status.txt
-    # echo sunrise $sunrise >>/Users/philip/theme-status.txt
-    # echo sunset $sunset >>/Users/philip/theme-status.txt
-    # echo midnight $midnight >>/Users/philip/theme-status.txt
-    # echo seeking $seeking >>/Users/philip/theme-status.txt
-    # echo cycle $cycle >>/Users/philip/theme-status.txt
-    # echo --- >>/Users/philip/theme-status.txt
-
-    if test "$seeking" != "$cycle"
-        and test $seeking = night
-        and test $diff -lt 0
-        and test $dist -le 3600
-        set seeking transition
-        set cycle transition
-    end
-
-    # echo $seeking >>/Users/philip/theme-status.txt
-    # echo $diff >>/Users/philip/theme-status.txt
-    # echo $cycle >>/Users/philip/theme-status.txt
 
     if begin
-            test "$seeking" = $cycle
-            and test "$system_mode" != $seeking
+            test "$current_theme" != $cycle
         end
-        echo $seeking >$HOME/.dotfiles/theme/current
+        echo $cycle >$HOME/.dotfiles/theme/current
     end
 
 end
