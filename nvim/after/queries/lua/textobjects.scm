54a9e961663075ba90ec62409dcee167ced9da19
Philip Gaudreau <philip.gaudreau@icloud.com>
2024-02-05 21:35:57 -0500

    add bool, dict, list, string

--- nvim/after/queries/lua/textobjects.scm
+++ nvim/after/queries/lua/textobjects.scm
@@ -1,3 +1,13 @@
 ; extends
 ; binary expression
 (binary_expression left: (_) @binary.lhs right: (_) @binary.rhs) @binary.outer
+
+; boolean value
+(true) @boolean
+(false) @boolean
+
+; table
+(table_constructor) @dictionary.outer @list.outer
+
+; string
+(string _ (string_content) @string.inner _) @string.outer
--- nvim/after/queries/python/textobjects.scm
+++ nvim/after/queries/python/textobjects.scm
@@ -1,3 +1,18 @@
 ; extends
 ; boolean operator
 (boolean_operator left: (_) @binary.lhs right: (_) @binary.rhs) @binary.outer
+
+; boolean value
+(true) @boolean
+(false) @boolean
+
+; dictionary
+(dictionary) @dictionary.outer
+(dictionary_comprehension) @dictionary.outer
+
+; list
+(list_comprehension) @list.outer
+(list) @list.outer
+
+; string
+(string _ (string_content) @string.inner _) @string.outer
