f13089145232eb3ed99b0ac9ce968dc31fb2ba66
Philip Gaudreau <philip.gaudreau@icloud.com>
2024-02-05 22:40:54 -0500

    `dVs` to delete linewise surround; `sn` `sVn` to operate on node; add treesitter objects; fix `k` (call) surround; `ss` to surround current line

--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -326,27 +326,49 @@ return {
 			{ "s", mode = { "n", "v" } },
 			{ "S", mode = { "n", "v" } },
 			{ "ds", mode = "n" },
+			{ "dVs", mode = "n" },
 			{ "cs", mode = "n" },
+			{ "sn", mode = "n" },
+			{ "sVn", mode = "n" },
 		},
 		config = function()
 			require("nvim-surround").setup({
+				move_cursor = false,
 				keymaps = {
 					insert = "<C-s>",
 					insert_line = "<C-g>s",
 					normal = "s",
-					normal_cur = "ss",
-					normal_line = "sc",
-					normal_cur_line = "sC",
+					normal_cur = false,
+					normal_line = "sV",
+					normal_cur_line = false,
 					visual = "s",
 					visual_line = "S",
+					delete_line = "dVs",
 					delete = "ds",
 					change = "cs",
+					change_line = "cS",
 				},
 				surrounds = {
-					["f"] = {},
+					["c"] = "conditional",
+					["f"] = "function",
+					["l"] = "loop",
 					["k"] = {
 						add = function()
-							api.nvim_feedkeys("i", "n", false)
+							vim.defer_fn(function()
+								local row, col = unpack(require("nvim-surround.buffer").get_mark("["))
+								api.nvim_win_set_cursor(0, { row, col - 1 })
+								local feed_prefix = require("nvim-surround.cache").normal.line_mode and "_" or ""
+								api.nvim_feedkeys(feed_prefix .. "i", "n", false)
+							end, 0)
+
+							api.nvim_create_autocmd("InsertLeave", {
+								once = true,
+								callback = function()
+									vim.go.operatorfunc = "v:lua.require'nvim-surround.utils'.NOOP"
+									api.nvim_feedkeys("g@l", "n", false)
+								end,
+							})
+
 							return { { "(" }, { ")" } }
 						end,
 						find = function()
@@ -374,6 +396,36 @@ return {
 			keymap.set("n", "S", function()
 				return "<Plug>(nvim-surround-normal)g_"
 			end, { expr = true, silent = true })
+
+			keymap.set("n", "ss", function()
+				local current_line = fn.line(".")
+
+				require("nvim-surround.buffer").highlight_selection({
+					first_pos = { current_line, api.nvim_get_current_line():find("[^%s]") or 0 },
+					last_pos = { current_line, vim.fn.col("$") },
+				})
+
+				local input = vim.fn.getcharstr()
+				if input == "V" then
+					return "<Plug>(nvim-surround-normal-line)L"
+				else
+					return "<Plug>(nvim-surround-normal-cur)" .. input
+				end
+			end, { expr = true, silent = true, remap = true })
+
+			keymap.set("n", "sn", function()
+				require("treemonkey").select({ ignore_injections = false })
+				if utils.get_visual_state().is_active then
+					api.nvim_feedkeys(vim.keycode("<esc>") .. "sv`<", "", false)
+				end
+			end, { silent = true })
+
+			keymap.set("n", "sVn", function()
+				require("treemonkey").select({ ignore_injections = false })
+				if utils.get_visual_state().is_active then
+					api.nvim_feedkeys(vim.keycode("<esc>") .. "sVv`<", "", false)
+				end
+			end, { silent = true })
 		end,
 	},
 	{
