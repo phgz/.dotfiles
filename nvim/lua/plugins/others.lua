6cb921f4f13cdba522a21eed0a065e6018e3eb14
Philip Gaudreau <philip.gaudreau@icloud.com>
2025-02-07 19:06:12 -0500

    add a CSV viewer

--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -667,6 +667,31 @@ return {
 		end,
 	},
 	{
+		"hat0uma/csvview.nvim",
+		ft = "csv",
+		config = function()
+			require("csvview").setup({
+				parser = { comments = { "#", "//" } },
+				keymaps = {
+					-- Text objects for selecting fields
+					textobject_field_inner = { "if", mode = { "o", "x" } },
+					textobject_field_outer = { "af", mode = { "o", "x" } },
+					-- Excel-like navigation:
+					-- Use <Tab> and <S-Tab> to move horizontally between fields.
+					-- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
+					jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+					jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+					jump_next_row = { "<Enter>", mode = { "n", "v" } },
+					jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+				},
+				view = {
+					display_mode = "border",
+				},
+			})
+			require("csvview").enable()
+		end,
+	},
+	{
 		"junegunn/vim-easy-align", -- Align text based on pattern
 		keys = { { "<localleader>a", mode = { "n", "x" } } },
 		config = function()
