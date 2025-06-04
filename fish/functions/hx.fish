ebda4ae30a3ca0ed93a6969380fa6fae37a59634
Philip Gaudreau <philip.gaudreau@icloud.com>
2025-06-04 15:12:57 -0400

    change theme heuristic so it is temporary

--- fish/functions/hx.fish
+++ fish/functions/hx.fish
@@ -1,4 +1,5 @@
 function hx --description "run helix with a random theme"
-    nu -c "open $HOME/.dotfiles/helix/config.toml | update theme $(_random_helix_theme (theme)) | save -f $HOME/.dotfiles/helix/config.toml"
-    command hx $argv
+    command hx --config (sed "1i\\
+    theme='$(_random_helix_theme (theme))'
+    " $HOME/.dotfiles/helix/config.toml | psub) $argv
 end
